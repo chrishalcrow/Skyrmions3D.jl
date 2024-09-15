4f362d7299355511245c58dcaccad22456518fea
Imzeep <imzee42@outlook.com>
2024-09-15 01:17:54 +0100

    irr

--- axialtest.jl
+++ axialtest.jl
@@ -7,18 +7,13 @@ using LinearAlgebra
 GLMakie.activate!()
 Makie.inline!(false)
 
-nuc = Skyrmion([60, 60, 60], [0.2, 0.2, 0.2], mpi = 0, Fpi = 108, ee = 4.84, boundary_conditions = "dirichlet")
 
-p1(z) = z
-q1(z) = 1
-f1(r) = 4 * atan(exp(-r))
-
-make_rational_map!(nuc, p1, q1, f1)
+nuc = deserialize("l120_B1M06.5")
 
 
 u = [0, 0, 1]  
 v = [1, 0, 0]  
-a = 0
+a = 30
 r = 10
 
 axial_symmetry_plot(nuc, u, v, a, r, points = 50, field=4)
